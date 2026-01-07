fa5da23890279c4c8f06924433aa2ac1313510ae
Karthik171819 <karthikkumar092004@gmail.com>
2026-01-07 17:22:42 +0530

    push

---
+++ backend/models/invoiceModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const ItemSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  qty: {
+    type: Number,
+    required: true,
+    default: 1
+  },
+  unitPrice: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  
+    _id: false
+  
+});
+
+
