9b3104f556130439c7d91d1ecc0b35674f1c2aa7
Karthik171819 <karthikkumar092004@gmail.com>
2026-01-11 15:52:33 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import 'dotenv/config';
 import { clerkMiddleware } from '@clerk/express'
 import {connectDB} from './config/db.js';
+import path from 'path';
+import invoiceRouter from './routes/invoiceRouter.js';
 
 const app = express();
 const port = 4000;
@@ -17,6 +19,10 @@ app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 connectDB();
 
 //Routes
+
+app.use('/uploads', express.static(path.join(process.cwd(), "uploads")));
+
+app.use('/api/invoice', invoiceRouter);
 
 app.get('/', (req, res) => {
     res.send('API is running...');  
