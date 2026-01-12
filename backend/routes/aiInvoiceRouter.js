1ac6b14ef95829710d1011e6b8ea3eb492d1d3d1
Karthik171819 <karthikkumar092004@gmail.com>
2026-01-12 15:04:25 +0530

    backend

---
+++ backend/routes/aiInvoiceRouter.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { GoogleGenAI } from "@google/genai";
+import dotenv from 'dotenv';
+
+dotenv.config();
+const aiInvoiceRouter = express.Router();
+
+const API_KEY = process.env.GEMINI_API_KEY;
+if (!API_KEY) {
+    console.warn("No Gemini Key found in the .env");
+}
