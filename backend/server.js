6b672ddf47bf068d2321edaf58ce3a1bcae1d3b9
Karthik171819 <karthikkumar092004@gmail.com>
2026-01-13 16:18:01 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const app = express();
 const port = 4000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173/",
+    credentials: true,
+}));
 app.use(express.json({limit: "20mb"}));
 app.use(clerkMiddleware());
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
