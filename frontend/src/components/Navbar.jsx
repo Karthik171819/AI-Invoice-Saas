361c28b602279ebdf4ddfa39732746f5e7fa28ce
Karthik171819 <karthikkumar092004@gmail.com>
2026-01-13 18:30:28 +0530

    backend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,69 @@
 import React from "react";
 import { navbarStyles } from "../assets/dummyStyles";
 import logo from "../assets/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useRef } from "react";
+import { useUser, useAuth, useClerk, SignedOut } from "@clerk/clerk-react";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const [profileOpen, setProfileOpen] = useState(false);
+
+  const { user } = useUser();
+  const { getToken, isSignedIn } = useAuth();
+  const clerk = useClerk();
+
+  //useNavigate from react router  dom package
+  const navigate = useNavigate();
+  const profileRef = useRef(null);
+  const TOKEN_KEY = "token";
+
+  // to open login model
+  function openSignIn() {
+    try {
+      if (clerk && typeof clerk.openSignIn === "function") {
+        clerk.openSignIn();
+      } else {
+        navigate("/login");
+      }
+    } catch (e) {
+        console.error("Error opening sign-in:", e);
+        navigate('/login');
+    }
+  }
+
   return (
     <header className={navbarStyles.navbar}>
       <div className={navbarStyles.container}>
         <nav className={navbarStyles.nav}>
           <div className={navbarStyles.logoSection}>
-            <Link to='/' className={navbarStyles.logoLink}>
-            <img src={logo} alt="logo" className={navbarStyles.logoImage}/>
-            <span className={navbarStyles.logoText}>InvoiceAI</span>
+            <Link to="/" className={navbarStyles.logoLink}>
+              <img src={logo} alt="logo" className={navbarStyles.logoImage} />
+              <span className={navbarStyles.logoText}>InvoiceAI</span>
             </Link>
+
+            <div className={navbarStyles.desktopNav}>
+              <a href="#features" className={navbarStyles.navLink}>
+                Features
+              </a>
+              <a href="#features" className={navbarStyles.navLinkInactive}>
+                Pricing
+              </a>
+            </div>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <div className={navbarStyles.authSection}>
+              <SignedOut>
+                <button
+                  onClick={openSignIn}
+                  className={navbarStyles.signInButton}
+                  type="button"
+                >
+                  Sign in
+                </button>
+              </SignedOut>
+            </div>
           </div>
         </nav>
       </div>
