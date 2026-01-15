338e8eedbaccc7a08e374dea72399c7b26909c18
Karthik171819 <karthikkumar092004@gmail.com>
2026-01-15 13:12:34 +0530

    frontend

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { heroStyles } from "../assets/dummyStyles";
+
+const Hero = () => {
+  return (
+    <section className={heroStyles.section}>
+      <div className={heroStyles.bgElement1}></div>
+      <div className={heroStyles.bgElement2}></div>
+      <div className={heroStyles.bgElement3}></div>
+
+      <div className={heroStyles.gridPattern}></div>
+      <div className={heroStyles.container}>
+        <div className={heroStyles.grid}>
+          <div className={heroStyles.containet}>
+            <div className={heroStyles.containerInner}>
+              <div classname={heroStyles.badge}>
+                <div className={heroStyles.badgeDot}></div>
+                <span className={heroStyles.badgeText}>
+                    AI-Powered Invoicing Platform 
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
