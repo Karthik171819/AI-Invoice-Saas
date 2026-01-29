ca654387de9d31fe26a460e808366c2d153f8acf
Karthik171819 <karthikkumar092004@gmail.com>
2026-01-29 14:04:22 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const port = 4000;
 
 // Middleware
 app.use(cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
     credentials: true,
 }));
 app.use(express.json({limit: "20mb"}));
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { dashboardStyles } from '../assets/dummyStyles.js'
+
+//backend connection
+const API_BASE = 'http://localhost:4000';
 
 const Dashboard = () => {
   return (
