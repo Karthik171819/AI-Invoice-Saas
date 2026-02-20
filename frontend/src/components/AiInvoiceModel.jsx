92dd8f88a0cf65dbdb0c1893866bdea1a8d89578
Karthik171819 <karthikkumar092004@gmail.com>
2026-02-20 11:32:43 +0530

    frontend

--- frontend/src/components/AiInvoiceModel.jsx
+++ frontend/src/components/AiInvoiceModel.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { aiInvoiceModelStyles } from "../assets/dummyStyles";
 import GeminiIcon from "./GeminiIcon";
 import AnimatedButton from "../assets/GenerateBtn/Gbtn";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const AiInvoiceModel = ({ open, onClose, onGenerate, initialText = "" }) => {
   const [text, setText] = useState(initialText || "");
@@ -43,7 +43,35 @@ const AiInvoiceModel = ({ open, onClose, onGenerate, initialText = "" }) => {
     }
   }
 
-  return <div>AiInvoiceModel</div>;
+  return (
+    <div className={aiInvoiceModelStyles.overlay}>
+      <div
+        className={aiInvoiceModelStyles.backdrop}
+        onClick={() => onClose && onClose()}
+      ></div>
+
+      <div className={aiInvoiceModelStyles.modal}>
+        <div className="flex items-start justify-between">
+          <div>
+            <h3 className={aiInvoiceModelStyles.title}>
+              <GeminiIcon className=" w-6 h-6 group-hover:scale-110 transition-transform flex-none" />
+              Create Invoice with AI.
+            </h3>
+            <p className={aiInvoiceModelStyles.description}>
+              Paste any text that contains invoice details like client, items,
+              qty, prices and we'll attempt to extract an invoice
+            </p>
+          </div>
+          <button
+            onClick={() => onClose && onClose()}
+            className={aiInvoiceModelStyles.closeButton}
+          >
+            ✕
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AiInvoiceModel;
