ce8143368dc59da7b1a4d53be3362aaef7b30c05
Karthik171819 <karthikkumar092004@gmail.com>
2026-02-21 15:15:34 +0530

    frontend

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <div classsName="text-center p-8">
+        <h1 className="text-4xl font-bold mb-2">404</h1>
+        <p className="text-gray-800 mb-6">Page Not Found</p>
+        <Link to="/" className="px-4 py-2 rounded-mb bg-indigo-700 text-white">
+          Go Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
